e427bd411f6f5caa292284a766fa3989139f7d11
Leonardo Tosin <leotosin99@hotmail.com>
2013-08-01 23:41:25 -0300

    Busca ta foda

--- lib/busca.php
+++ lib/busca.php
@@ -4,13 +4,13 @@
 
 	#Instancia objeto que vai tratar o banco de dados dessa pagina
 	$banco = new bancoanime;
-	if(isset($_POST['pesquisa'])){
-		$pesquisa = $_POST['pesquisa'];
-	} else
+
+	$pesquisa = $_POST['pesquisa'];
+
 	if(isset($pesquisa)){
 		$result = $banco->ExisteAnime($pesquisa);
 		$pesquisa = mysql_fetch_assoc($result);
-		$Conteudo = $banco->RedirecionaPara('anime/'.$pesquisa);
+		$Conteudo = $banco->RedirecionaPara('anime/'.$pesquisa['ANIMENOME']);
 	} else {
 		$Conteudo = $banco->CarregaHtml('busca');
 	}
